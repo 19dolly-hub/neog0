9c13b7e8ad8d03843ef4820132a66b02409b1a98
19dolly-hub <dollyraghuwanshi16@gmail.com>
2023-11-26 14:30:09 +0530

    feat: add loader

--- Pirate-Translator/script.js
+++ Pirate-Translator/script.js
@@ -1,19 +1,26 @@
 const input = document.getElementById("input");
 const output = document.getElementById("output");
 const translate = document.getElementById("translate");
+const loader = document.getElementById("loader");
 const url = "https://api.funtranslations.com/translate/pirate.json";
 
 translate.addEventListener("click", () => {
     if (input.value !== "") {
         const fetchUrl = encodeUrl(input.value);
 
+        loader.style.display = "block";
+
         fetch(fetchUrl)
         .then(response => response.json())
         .then(data => {
             output.textContent = data.contents.translated;
         })
-        .catch(() => {
+        .catch((err) => {
+            console.log(err);
             output.textContent = "Some error occured! Please try again after sometime."
+        })
+        .finally(function() {
+            loader.style.display = "none";
         });
     }
 });
