d4c407543e909e7e8d78396d4ff196ba4ab224ec
19dolly-hub <dollyraghuwanshi16@gmail.com>
2023-11-27 10:56:43 +0530

    update script.js

--- cash-register/script.js
+++ cash-register/script.js
@@ -4,21 +4,24 @@ const cal = document.getElementById("calculate");
 
 const outputs = document.querySelectorAll(".output");
 
-const notes = [2000, 500, 100, 20, 10, 5, 1];
-let requiredObj = {};
+const notes = [2000, 500, 100, 20, 10, 5, 1]; // eligible notes
+let requiredObj = {}; // initialize an obj for containing the info of required notes
+
 
 cal.addEventListener("click", function() {
     const billAmt = bill.value;
     const given = cashAmt.value;
 
-    if (given === "" || billAmt === "") {
+    // if fields are empty, alert
+    if (given.trim() === "" || billAmt.trim() === "") {
         alert("Please fill out both the fields to calculate!!");
     }
 
+    // if more than total bill is paid, return notes
     if (+given >= +billAmt) {
-        const totalNotes = notes.length;
+        const totalNotes = notes.length; // eligible notes
 
-        let leftAmt = given - billAmt;
+        let leftAmt = given - billAmt; // amount to be returned
         let divisor;
         let numOfNote;
 
@@ -26,24 +29,25 @@ cal.addEventListener("click", function() {
 
         function loop(leftAmt) {
             if (leftAmt === 0) return;
+
             for(let i=0; i<totalNotes; i++) {
-                if(notes[i] < leftAmt) {
+                if(notes[i] < leftAmt) { // first smaller(value) note
                     divisor = notes[i];
                     break;
                 }
             }    
-            numOfNote = Math.floor(leftAmt / divisor);
-            leftAmt = leftAmt % (divisor * numOfNote);
+            numOfNote = Math.floor(leftAmt / divisor); // no. of notes = left amount / one note value
+            leftAmt = leftAmt % (divisor * numOfNote); // amount left after = remainder of (left amt / paid notes total value)
 
-            requiredObj[divisor] = numOfNote;
-            loop(leftAmt);
+            requiredObj[divisor] = numOfNote; // adding the info of notes and count in the obj
+            loop(leftAmt); // repeating until zero left
         }
 
         outputs.forEach(td => {
-            td.textContent = requiredObj[Number(td.id)] || "-";
+            td.textContent = requiredObj[Number(td.id)] || "-"; // render respectively and "-" for null/undefined
         });
 
-    }
+    } // if less than total bill is paid, ask for paying correct amount
     else if (+given < +billAmt) {
         alert(`Customer has given ${billAmt - given} rs. less cash! Tell him to pay the complete bill amount.`);
     }
